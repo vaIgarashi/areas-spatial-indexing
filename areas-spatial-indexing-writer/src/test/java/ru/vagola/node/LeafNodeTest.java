49765dc1bee4ab8893329899191420ae6d37da7c
Vladislav Golub <vladislavs.golubs@yandex.ru>
2019-11-16 12:05:17 +0300

    Add test for leaf node write

--- areas-spatial-indexing-writer/src/test/java/ru/vagola/node/LeafNodeTest.java
+++ areas-spatial-indexing-writer/src/test/java/ru/vagola/node/LeafNodeTest.java
@@ -1,10 +1,17 @@
 package ru.vagola.node;
 
+import com.google.common.io.ByteArrayDataInput;
+import com.google.common.io.ByteArrayDataOutput;
+import com.google.common.io.ByteStreams;
 import org.junit.Test;
 import ru.vagola.Area;
 import ru.vagola.BoundingBox;
 import ru.vagola.Point;
 import ru.vagola.QuadTreeConfig;
+
+import java.io.IOException;
+
+import static org.junit.Assert.assertEquals;
 
 public class LeafNodeTest {
 
@@ -31,6 +38,30 @@ public class LeafNodeTest {
         for (short i = 0; i < 127; i++) {
             Area area = new Area(i, areaBoundingBox);
             leafNode.putArea(area);
+        }
+    }
+
+    @Test
+    public void testWriteToBinary() throws IOException {
+        BoundingBox nodeBoundingBox = new BoundingBox(new Point(0, 0), new Point(100, 100));
+        LeafNode leafNode = new LeafNode(nodeBoundingBox, new QuadTreeConfig());
+
+        BoundingBox areaBoundingBox = new BoundingBox(new Point(25, 25), new Point(75, 75));
+
+        for (short i = 0; i < 127; i++) {
+            Area area = new Area(i, areaBoundingBox);
+            leafNode.putArea(area);
+        }
+
+        ByteArrayDataOutput output = ByteStreams.newDataOutput();
+        leafNode.writeToBinary(output);
+
+        ByteArrayDataInput input = ByteStreams.newDataInput(output.toByteArray());
+
+        assertEquals(254, input.readByte() & 0xFF);
+
+        for (short i = 0; i < 127; i++) {
+            assertEquals(i, input.readShort());
         }
     }
 
